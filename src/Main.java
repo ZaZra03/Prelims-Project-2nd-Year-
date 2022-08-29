d56530e1ad0bdee6df1ecb67715d34fad4904963
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2022-08-29 23:18:52 +0800

    Removed spaces and unnecessary lines of code

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,24 @@ public class Main {
 		
 		
 		do {
-			
 			System.out.println("WELCOME TO SUMMONERS RIFT");
 			System.out.println();
-			
 			System.out.println("Select Your Transaction \n" + "A. Register \n"+ "B. Exit \n");
 			System.out.println();
 			
 			char choice1 = rd.readLine().charAt(0);
-			
+
 			if(choice1 == 'A' || choice1 == 'a') {
-				
 				System.out.println("Choose what to record in your pet's Information");
 				System.out.println("1. Pet Name and Type (Dog or Cat) ");
 				System.out.println("2. Pet Name, Type (Dog or Cat), and Breed");
 				System.out.println("3. Pet Name, Type (Dog or Cat), Breed, and Color ");
 				System.out.println("4. Pet Name, Type (Dog or Cat), Breed, Color, and Age");
 				System.out.println("5. Pet Name, Type (Dog or Cat), Breed, Color, Age, and Sex");
-				System.out.println("6. Pet Name, Type (Dog or Cat), Breed, Color, Age, Sex, and Owners Name");
-				
+				System.out.println("6. Pet Name, Type (Dog or Cat), Breed, Color, Age, Sex, and Owners Name");				
 				char choice2 = rd.readLine().charAt(0);
 				
 				if(choice2 == '1') {
-					System.out.println();
 					System.out.println("Enter your pet's name: ");
 					String name = rd.readLine();
 					
@@ -38,10 +33,9 @@ public class Main {
 					
 					Pet pet1 = new Pet(name, type);
 					pet1.ShowDetails();
-					
-					}else if(choice2 == '2') {
-						System.out.println();
-						
+					} 
+				
+				else if(choice2 == '2') {					
 						System.out.println("Enter your pet's name: ");
 						String name = rd.readLine();
 						
@@ -56,9 +50,9 @@ public class Main {
 					
 					
 					
-					}else if(choice2 == '3') {
-						System.out.println();
-						
+					} 
+				
+				else if(choice2 == '3') {
 						System.out.println("Enter your pet's name: ");
 						String name = rd.readLine();
 						
@@ -74,9 +68,9 @@ public class Main {
 						Pet pet3 = new Pet(name, type, breed, color);
 						pet3.ShowDetails();
 						
-					}else if(choice2 == '4') {
-						System.out.println();
-						
+					} 
+				
+				else if(choice2 == '4') {					
 						System.out.println("Enter your pet's name: ");
 						String name = rd.readLine();
 						
@@ -95,9 +89,9 @@ public class Main {
 						Pet pet4 = new Pet(name, type, breed, color, age);
 						pet4.ShowDetails();
 						
-					}else if(choice2 == '5') {
-						System.out.println();
-						
+					} 
+				
+				else if(choice2 == '5') {
 						System.out.println("Enter your pet's name: ");
 						String name = rd.readLine();
 						
@@ -117,11 +111,10 @@ public class Main {
 						String sex = rd.readLine();
 						
 						Pet pet5 = new Pet(name, type, breed, color, age, sex);
-						pet5.ShowDetails();
-						
-					}else if(choice2 == '6') {
-						System.out.println();
-						
+						pet5.ShowDetails();						
+					} 
+				
+				else if(choice2 == '6') {				
 						System.out.println("Enter your pet's name: ");
 						String name = rd.readLine();
 						
@@ -144,29 +137,22 @@ public class Main {
 						String petOwnersName = rd.readLine();
 						
 						Pet pet6 = new Pet(name, type, breed, color, age, sex, petOwnersName);
-						pet6.ShowDetails();
-						
-					}else {
-						System.out.println();
-						
-						System.out.println("Invalid Input");
+						pet6.ShowDetails();						
 					}
 				
+				else System.out.println("Invalid Input");
 					
-			}else if(choice1 == 'B' || choice1 == 'b') {
-				System.out.println();
 				
-				System.out.println("System Exit");
-				
+					
+			} 
+			
+			else if(choice1 == 'B' || choice1 == 'b') {			
+				System.out.println("System Exit");	
 				break;
-			}else {
-				System.out.println();
-				
-				System.out.println("Invalid Input \n");
-			}
+			} 
+			
+			else System.out.println("Invalid Input \n");
 		
-		}while(true);
-
+		} while(true);
 	}
-
 }
--- src/Pet.java
+++ src/Pet.java
@@ -9,22 +9,17 @@ public class Pet{
 	public String petOwnersName;
 
 	public Pet(String name, String type) {
-
 		this.petName = name; 
 		this.petType = type;
-
-
 	}
 
 	public Pet(String name, String type, String breed){
-
 		this.petName = name; 
 		this.petType = type;
 		this.petBreed = breed;
 	}
 
 	public Pet(String name, String type, String breed, String color){
-
 		this.petName = name; 
 		this.petType = type;
 		this.petBreed = breed;
@@ -32,7 +27,6 @@ public class Pet{
 	}
 
 	public Pet(String name, String type, String breed, String color, String age){
-
 		this.petName = name; 
 		this.petType = type;
 		this.petBreed = breed;
@@ -41,18 +35,15 @@ public class Pet{
 	}
 
 	public Pet(String name, String type, String breed, String color, String age, String sex){
-
 		this.petName = name; 
 		this.petType = type;
 		this.petBreed = breed;
 		this.petColor = color;
 		this.petAge = age;
 		this.petSex = sex;
-
 	}
 
 	public Pet(String name, String type, String breed, String color, String age, String sex, String petOwnersName){
-
 		this.petName = name; 
 		this.petType = type;
 		this.petBreed = breed;
@@ -60,11 +51,9 @@ public class Pet{
 		this.petAge = age;
 		this.petSex = sex;
 		this.petOwnersName = petOwnersName;
-
 	}
 
 	public void ShowDetails() {
-
 		System.out.println("Your pet's name is : " + this.petName);
 		System.out.println("Your pet's type is : " + this.petType);
 		System.out.println("Your pet's breed is : " + this.petBreed);
@@ -73,5 +62,4 @@ public class Pet{
 		System.out.println("Your pet's sex is : " + this.petSex);
 		System.out.println("Your pet's owner name is : " + this.petOwnersName);	
 	}
-
 }
