d0a407bf657bfb8d1ad29e2ffb0f8ef0c3d73c3a
Sansr1val <110670707+Sansr1val@users.noreply.github.com>
2022-09-08 17:24:45 +0800

    Camel case redo

--- src/Pet.java
+++ src/Pet.java
@@ -58,7 +58,7 @@ public class Pet {
 	}
 	
 	//Method to show the details the object has
-	public void showDetails() {
+	public void ShowDetails() {
 		//Informations that are not filled in/unavailable is not shown.
 		if(name != null) {
 			System.out.println("Pet name: "+name);
